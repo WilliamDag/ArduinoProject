7a7da657cc0a08336f37c6d4f77243544a695e0a
WilliamDag <32744303+WilliamDag@users.noreply.github.com>
2018-03-16 12:49:36 +0000

    Initial Voice Control
    Added initial voice control via bluetooth HC-05 module. Can turn on and off digital port 13 for an LED etc.

---
+++ Voice_Activation.ino
@@ -0,0 +1,24 @@
+String voice;
+int led13 = 13;
+
+void setup() {
+  Serial.println("Initializing...");
+  Serial.begin(115200);
+  pinMode(led13, OUTPUT);
+}
+//-----------------------------------------------------------------------//  
+void loop() {
+  while (Serial.available()){  //Check if there is an available byte to read
+  delay(10); //Delay added to make thing stable 
+  char c = Serial.read(); //Conduct a serial read
+  if (c == '#') {break;} //Exit the loop when the # is detected after the word
+  voice += c; //Shorthand for voice = voice + c
+  }  
+  if (voice.length() > 0) {
+    //Serial.println("Voice detected!");
+    Serial.println(voice); 
+//-----------------------------------------------------------------------//    
+  if(voice == "on") {digitalWrite(led13, HIGH);}
+  else if(voice == "off") {digitalWrite(led13, LOW);}
+//-----------------------------------------------------------------------//  
+voice="";}} //Reset the variable after initiating
