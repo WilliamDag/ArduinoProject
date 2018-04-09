51c457c06a75f271ce6ef8d2eceea71385639f92
ScottBain96 <32749929+ScottBain96@users.noreply.github.com>
2018-04-09 15:40:33 +0100

    added bitcoin function

--- Voice_Activation.ino
+++ Voice_Activation.ino
@@ -5,12 +5,44 @@
 //Might need for json btc price extraction
 //const String url = "http://api.coindesk.com/v1/bpi/currentprice.json";
 
+void bitcoin() {
+  const char* host = "api.coindesk.com";
+  WiFiClient client;
+  const int httpPort = 80;
+  
+  if (!client.connect(host,httpPort)){
+    
+   Serial.println("Can't connect to server");
+    return;
+    
+  }
+  
+  string url = "v1/bpi/currentprice.json";
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  "Host: " + host + "\r\n" +
+  "Connection: close\r\n\r\n");
+  
+  //Reading the answer
+  
+  String answer;
+  while (client.available()){
+   String line = client.readStringUntil('\r');
+    answer += line;
+    
+  }
+  
+  client.stop();
+  Serial.println();
+  Serial.println("Connection Closed");
+  
+}
+
 
 String voice;
 int led13 = 13;
 //Might be needed for extracting prices from api
-//#include <ArduinoJson.h>
-
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
 void setup() {
   Serial.println("Initializing...");
   Serial.begin(115200);
